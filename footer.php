75eadebf4c455721468463a86fb7139f2c14d9ae
Matthew Flanagan <mattimustang@gmail.com>
2016-02-12 20:25:26 +1100

    Updated for Tripod 4.1

--- footer.php
+++ footer.php
@@ -14,14 +14,13 @@
     <?php //} ?>
     </div>
     
-    
     <div class="overlay">&nbsp;</div>
     <?php //if(is_singular()){ ?>
         <script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
         <script type="text/javascript">
             (function() {
                 var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-                po.src = 'https://apis.google.com/js/plusone.js';
+                po.src = '//apis.google.com/js/plusone.js';
                 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
             })();
         </script>
